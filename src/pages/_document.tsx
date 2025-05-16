7f74f3885320011cec018d459cc379d4c850ff2b
Hendrick Sánchez <hsanchez@membranelabs.com>
2025-05-16 11:15:28 -0300

    Add the initial project structure

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import "@/shared/styles/globals.css";
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+// src/app/page.tsx
+// import SearchBox from "@/modules/items/presentation/components/SearchBox";
+
+export default function HomePage() {
+  return (
+    <section>
+      Hereeeee
+      {/* <SearchBox /> */}
+    </section>
+  );
+}
